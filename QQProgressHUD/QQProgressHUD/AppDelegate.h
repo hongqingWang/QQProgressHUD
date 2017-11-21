a1c58ace1089aeef19c1263ea936ee1e687f5850
wanghongqing <272338444@qq.com>
2017-11-21 13:55:59 +0800

    初始化代码

---
+++ QQProgressHUD/QQProgressHUD/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QQProgressHUD
+//
+//  Created by Mac on 2017/11/21.
+//  Copyright © 2017年 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
