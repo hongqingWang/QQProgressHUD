40f64b13d0d7419c3e9f5b2443203adcd0c4cba1
wanghongqing <272338444@qq.com>
2017-11-21 17:30:24 +0800

    常用的已搞定

--- QQProgressHUD/QQProgressHUD/ViewController.m
+++ QQProgressHUD/QQProgressHUD/ViewController.m
@@ -7,8 +7,14 @@
 //
 
 #import "ViewController.h"
+#import "MBProgressHUD+QQ.h"
 
-@interface ViewController ()
+@interface ViewController ()<UITableViewDataSource, UITableViewDelegate>
+
+/** TableView */
+@property (nonatomic, strong) UITableView *tableView;
+/** 标题 */
+@property (nonatomic, strong) NSArray *titles;
 
 @end
 
@@ -16,14 +22,118 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.navigationItem.title = @"首页";
+    
+    [self.view addSubview:self.tableView];
 }
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma mark - UITableViewDataSource
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    
+    return self.titles.count;
 }
 
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    static NSString *cellId = @"cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellId];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellId];
+    }
+    
+    cell.textLabel.text = self.titles[indexPath.row];
+    
+    return cell;
+}
+
+#pragma mark - UITableViewDelegate
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    
+    switch (indexPath.row) {
+        case 0:
+        {
+            // 文字
+            [MBProgressHUD qq_showText:@"加载成功..."];
+        }
+            break;
+        case 1:
+        {
+            // 指示器
+            [MBProgressHUD qq_showIndicator];
+        }
+            break;
+        case 2:
+        {
+            // 文字 + 指示器
+            [MBProgressHUD qq_showTextAndIndicator:@"正在加载..."];
+            
+            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+                
+                // 隐藏
+                [MBProgressHUD qq_hideHUD];
+            });
+        }
+            break;
+        case 3:
+        {
+            // 成功图片
+            [MBProgressHUD qq_showSuccess];
+        }
+            break;
+        case 4:
+        {
+            // 成功图片 + 文字(false)
+            [MBProgressHUD qq_showSuccess:@"修改成功!"];
+        }
+            break;
+        case 5:
+        {
+            // 失败图片
+            [MBProgressHUD qq_showError];
+        }
+            break;
+        case 6:
+        {
+            // 失败图片 + 文字(false)
+            [MBProgressHUD qq_showError:@"网络出错!"];
+        }
+            break;
+            
+        default:
+            break;
+    }
+}
+
+#pragma mark - Getters and Setters
+- (UITableView *)tableView {
+    
+    if (_tableView == nil) {
+        
+        CGRect frame = CGRectMake(0, 64, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height - 64);
+        _tableView = [[UITableView alloc] initWithFrame:frame style:UITableViewStylePlain];
+        _tableView.dataSource = self;
+        _tableView.delegate = self;
+    }
+    return _tableView;
+}
+
+- (NSArray *)titles {
+    
+    if (_titles == nil) {
+        
+        _titles = @[
+                    @"文字",
+                    @"指示器",
+                    @"文字 + 指示器",
+                    @"成功图片",
+                    @"成功图片 + 文字(false)",
+                    @"失败图片",
+                    @"失败图片 + 文字(false)"
+                    ];
+    }
+    return _titles;
+}
 
 @end
